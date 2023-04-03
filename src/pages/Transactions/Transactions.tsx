bfb219b645f46fb13dd92bd5946a036fc39ad9ee
Nathallye Bacelar <nathallyet@gmail.com>
2023-04-03 00:18:48 -0300

    Transactions component

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,11 +1,56 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 1.200,00</PriceHighlight>
+              </td>
+              <td>Casa</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Computador</td>
+              <td>
+                <PriceHighlight variant="income">R$ 5.400,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>15/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
