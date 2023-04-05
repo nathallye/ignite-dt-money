776e65a7f7705b4625902f351f28140afb2bd9e1
nathallye <nathallyet@gmail.com>
2023-04-05 03:18:44 -0300

    Buttons income and outcome

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -76,3 +76,35 @@ export const CloseButton = styled(Dialog.Close)`
   cursor: pointer;
   color: ${props => props.theme["gray-500"]};
 `;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  background: ${props => props.theme["gray-700"]};
+  border: 0;
+  border-radius: 6px;
+  color: ${props => props.theme["gray-300"]};
+  
+  gap: 0.5rem;
+  padding: 1rem;
+  
+  cursor: pointer;
+  
+  svg {
+    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+  }
+`;
