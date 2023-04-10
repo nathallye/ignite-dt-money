24e56b6424bb78a4dca37b68e43fbdd42c2f75eb
Nathallye Bacelar <nathallyet@gmail.com>
2023-04-10 19:24:40 -0300

    Update import index.ts NewTransactionModal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
-import { NewTransactionModal } from "../NewTransactionModal/NewTransactionModal";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export const Header = () => {
   return (
